a705a5ab40aebadc620e971f3edf53e34fd0e825
haoyuan.huhy <a7758526@gmail.com>
2015-07-21 09:02:56 +0800

    changes

--- tick.py
+++ tick.py
@@ -27,21 +27,25 @@ def get_ticks(id, end, delta):
             ticks = tick
         else:
             ticks = ticks.append(tick)
+        ticks['dt'] = ticks['date'] + ' '+ ticks['time']
     return ticks
 
 
-def cmp_top(id, ticks):
+def cmp_top(ticks):
     if ticks is None:
         print 'no'
         return None
-    today = ticks.tail(1)
+    try:
+        today = ts.get_today_ticks(id).sort('amount', ascending=False)
+    except:
+        return None
     sticks = ticks.sort('amount', ascending=False).head(20)
     sticks = sticks.reset_index()
     today = today.reset_index()
     top = sticks.ix[0]
     top_today = today.ix[0]
     
-    return [str(round((top.price - top_today.price)/top.price, 3)), top.price, top_today.price, top.amount, top_today.amount]
+    return [(round((top.price - top_today.price)/top.price, 3)), round(top.price, 3), round(top_today.price, 3), top.amount, top_today.amount]
 
 rs = list()
 cnt = 0
@@ -52,14 +56,14 @@ for id in ts.get_stock_basics().index:
     print id
     cnt += 1
     ticks = get_ticks(id, end_str, 100)
-    cmp = cmp_top(id, ticks)
+    cmp = cmp_top(ticks)
     if cmp is None:
         continue
     cmp.append(id)
     print cmp
     if count % 5 == 0:
         f.flush()
-    f.write(','.join(cmp) + '\n')
+    f.write(str(cmp) + '\n')
     rs.append(cmp)
 f.close()
 
